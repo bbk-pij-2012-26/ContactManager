ae1bc27e734a48bdd692ef9a75063ec181d335e9
bbk-pij-2012-26 <emanueleduardocarneiro@gmail.com>
2013-01-30 23:49:35 +0000

    created basic implementations of the interfaces

---
+++ src/domain/ContactImp.java
@@ -0,0 +1,25 @@
+package domain;
+import base.Contact;
+
+public class ContactImp implements Contact {
+
+	public int getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getNotes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void addNotes(String note) {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ src/domain/PastMeetingImp.java
@@ -0,0 +1,35 @@
+package domain;
+
+import java.util.Calendar;
+import java.util.Set;
+
+import base.Contact;
+import base.PastMeeting;
+
+public class PastMeetingImp implements PastMeeting {
+
+	@Override
+	public int getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Calendar getDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Contact> getContacts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getNotes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
