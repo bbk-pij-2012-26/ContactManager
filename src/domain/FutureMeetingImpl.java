23460afd42424353a48ad73ae5b5fe166a1f65f7
Emanuel Carneiro <emanueleduardosousacarneiro@gmail.com>
2013-01-31 19:11:19 +0000

    renamed files from 'ClassImp' to 'ClassImpl' to match CW spec (was missing the 'l' from the 'Imp')

---
+++ src/domain/FutureMeetingImpl.java
@@ -0,0 +1,29 @@
+package domain;
+
+import java.util.Calendar;
+import java.util.Set;
+
+import base.Contact;
+import base.FutureMeeting;
+
+public class FutureMeetingImpl implements FutureMeeting {
+
+	@Override
+	public int getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Calendar getDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Contact> getContacts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
